0e63e855904feeb0b4a75a8a22e18df13e085bdc
Far0 <40454984+Far0@users.noreply.github.com>
2018-06-23 02:23:25 +0300

    Update move.ps1

--- move.ps1
+++ move.ps1
@@ -1,3 +1,3 @@
-﻿[string]$sourceDirectory  = C:\Picidae_ADB\platform-tools\*
-[string]$destinationDirectory = C:\Picidae_ADB\
+﻿[string]$sourceDirectory  = "C:\Picidae_ADB\platform-tools\*"
+[string]$destinationDirectory = "C:\Picidae_ADB\"
 Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
