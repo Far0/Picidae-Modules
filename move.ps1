6c83040a86d763187a8c25b2f3a1ac9bc4d972de
Far <->
2018-06-23 01:42:13 +0300

    Initial commit

---
+++ move.ps1
@@ -0,0 +1,3 @@
+﻿[string]$sourceDirectory  = C:\Picidae_ADB\platform-tools\*
+[string]$destinationDirectory = C:\Picidae_ADB\
+Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
